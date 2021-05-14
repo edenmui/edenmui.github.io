ed2423fab22caa5f2bfefd9ea1b26eb1d9159619
Emporiusm <at19eden@gmail.com>
2021-05-15 01:49:29 +0800

    --- Auto-Git Commit ---

--- get_swift.py
+++ get_swift.py
@@ -1,9 +1,14 @@
 # %%
 doc = 'Assets/BIC.csv'
 import pandas as pd
+from pandas.core.frame import DataFrame
 
 df = pd.read_csv(doc)
-df['SWIFT_BIC11'] = df['SWIFT_BIC8'] + df['BRANCH']
+df.columns = ['ID', 'ISO Country Code', 'Country Name', 'Bank Name', 'City', 'Branch', 'SWIFT' ]
 # %%
+df.fillna('-', inplace=True)
+import numpy as np
+df['Type'] = df['Branch'].apply(lambda x: 'H' if x == '-' else 'B')
 df
+# %%
 # %%
