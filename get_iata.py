7eeaad9a143a525242165817784923967fbb213f
Emporiusm <at19eden@gmail.com>
2021-05-14 15:06:13 +0800

    3rd  commit

--- get_iata.py
+++ get_iata.py
@@ -5,10 +5,14 @@ df = pd.read_csv(doc)
 df
 # %%
 df = df[df['type'] != 'closed']
-# %%
-df
-# %%
-df.to_json?
+
 # %%
 df.to_json('iata.json', orient='index')
+# %%
+iata = df[['iata_code', 'name']].dropna()
+# %%
+
+iata['Airport'] = iata['iata_code'] + ' - ' + iata['name']
+ # %%
+iata['Airport'].to_json('iata.json', orient='index')
 # %%
