0f2a1444a177768e549b8ac4defd29123be600c3
Emporiusm <at19eden@gmail.com>
2021-05-14 15:15:58 +0800

    commit

--- get_iata.py
+++ get_iata.py
@@ -11,8 +11,8 @@ df.to_json('iata.json', orient='index')
 # %%
 iata = df[['iata_code', 'name']].dropna()
 # %%
+iata.columns = ['IATA', 'Name']
 
-iata['Airport'] = iata['iata_code'] + ' - ' + iata['name']
- # %%
-iata['Airport'].to_json('iata.json', orient='index')
+# %%
+iata.to_json('iata.json', orient='columns')
 # %%
