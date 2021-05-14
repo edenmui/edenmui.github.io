19327f41f563155acad39d98ab45e8bb96ac886a
Emporiusm <at19eden@gmail.com>
2021-05-14 15:54:14 +0800

    commit

--- get_iata.py
+++ get_iata.py
@@ -7,14 +7,25 @@ df
 df = df[df['type'] != 'closed']
 
 # %%
-df.to_json('iata.json', orient='index')
-# %%
 iata = df[['iata_code', 'name']].dropna()
 # %%
 iata.columns = ['IATA', 'Name']
 iata['ID'] = range(0,iata.shape[0])
-iata.set_index('ID', drop=True, inplace=True)
+iata.set_index('ID', drop=False, inplace=True)
 # %%
 iata.to_json('iata.json', orient='index')
+
 # %%
+iata
+# %%
+lst = []
+for iata, name, id in zip(iata['IATA'], iata['Name'], iata['ID']):
+    lst.append(dict({"ID":id, "IATA":iata, "Name":name}))
+# %%
+import json
+with open('iata.json', 'w') as js:
+    content = json.dumps(lst)
+    js.write(content)
+# %%
+
 # %%
