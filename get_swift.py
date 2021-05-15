f604e931401d31405b1f8a2f35cfd6418d708f08
Emporiusm <at19eden@gmail.com>
2021-05-15 13:13:02 +0800

    commit

--- get_swift.py
+++ get_swift.py
@@ -1,7 +1,7 @@
 # %%
 doc = 'Assets/swift.csv'
 import pandas as pd
-from pandas.core.frame import DataFrame
+
 
 df = pd.read_csv(doc)
 df.columns = ['ID', 'ISO Country Code', 'Country Name', 'Bank Name', 'City', 'Branch', 'SWIFT' ]
@@ -13,15 +13,18 @@ df['Type'] = df['Branch'].apply(lambda x: 'H' if x == '-' else 'B')
 
 df.set_index('SWIFT', inplace=True, drop=False)
 
-df['SWIFT + NAME'] = df['SWIFT'] + ' - ' + df['Bank Name'] + ' (' + df['Branch'] + '), ' + df['City'] + ', ' + df['Country Name']
+df['SWIFT + NAME'] = df['SWIFT'] + ' - ' + df['Bank Name']
 
 import json
 
 lst = []
-for swift, swift_name in zip(df['SWIFT'], df['SWIFT + NAME']):
-    lst.append({"SWIFT":swift, "BANK": swift_name})
+for id, swift_name in zip(df['ID'], df['SWIFT + NAME']):
+    lst.append({"ID":id, "BANK": swift_name})
 
 content = json.dumps(lst, ensure_ascii=False)
 with open('swift.json', 'w', encoding='utf-8') as js:
     js.write(content)
+# %%
 
+
+# %%
