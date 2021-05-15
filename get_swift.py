e0f044119ccfc0d9197ccc81bdb4c73522c0f77b
Emporiusm <at19eden@gmail.com>
2021-05-15 13:50:41 +0800

    --- Auto-Git Commit ---

--- get_swift.py
+++ get_swift.py
@@ -15,7 +15,11 @@ df.set_index('SWIFT', inplace=True, drop=False)
 
 df['SWIFT + NAME'] = df['SWIFT'] + ' - ' + df['Bank Name']
 
+df = df.apply(lambda x: x.apply(lambda y: str(y).title()))
 import json
+banned = '''AD AI AG AW BS BH BZ BM BW VG KY CK CY DM ET GH GI GD GG IR IM JE LR LI MT MH MC MS NR BQ NU PA WS SM RS SC LC KN VC'''
+banned = banned.split(' ')
+df = df.query("`ISO Country Code` not in @banned")
 
 lst = []
 for id, swift_name in zip(df['ID'], df['SWIFT + NAME']):
@@ -26,5 +30,6 @@ with open('swift.json', 'w', encoding='utf-8') as js:
     js.write(content)
 # %%
 
-dict(zip(df['ISO Country Code'], df['Country Name']))
+
+# %%
 # %%
