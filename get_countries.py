1424d16360829935de99f6885a7134b425c4bbc8
Emporiusm <at19eden@gmail.com>
2021-05-18 22:21:51 +0800

    commit

--- get_countries.py
+++ get_countries.py
@@ -2,6 +2,7 @@
 doc = 'Assets/countries.csv'
 import pandas as pd
 from pandas.core.indexes.base import ensure_index
+
 df0 = pd.read_csv(doc)
 
 df = df0[['code', 'name', 'id']].copy()
@@ -12,8 +13,9 @@ for id, name in zip(df['code'], df['Countries']):
     lst.append({"ID": f"{str(id).zfill(6)}", "Code - Country": name})
 
 import json
+
 content = json.dumps(lst,ensure_ascii=False)
-with open('country_codes.json', 'w', encoding='utf-8') as js:
+with open('country_codes.json', 'w') as js:
     js.write(content)
 
 # %%
