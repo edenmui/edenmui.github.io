8c0279b67ed2a56e0f5c7830cd7db64c7e2bd14b
Emporiusm <at19eden@gmail.com>
2021-05-18 18:36:15 +0800

    commit

--- get_countries.py
+++ get_countries.py
@@ -2,23 +2,13 @@
 doc = 'Assets/countries.csv'
 from json import encoder
 import pandas as pd
-
 df = pd.read_csv(doc, )
-
-# %%
-
-filtered = df[['code', 'name', 'id']]
-# %%
-filtered['name'] = filtered['name'].apply(str.title).apply(lambda x: x.replace('.',''))
-# %%
-
-filtered['name'] = filtered['code'] + ' - ' + filtered['name']
+filtered = df[['code', 'name', 'id']].copy()
+filtered['Countries'] = filtered['code'] + ' - ' + filtered['name']
 lst = []
-for id, name in zip(filtered['id'], filtered['name']):
-    lst.append({"ID": id, "code": name})
-# %%
+for id, name in zip(range(0,filtered.shape[0]), filtered['Countries']):
+    lst.append({"ID": id, "Countries": name})
 import json
-
 content = json.dumps(lst, ensure_ascii=False)
 with open('iso3166.json', 'w', encoding='utf-8') as js:
     js.write(content)
