bd40980d399c3b801d63959de3e427cc2d2c67df
Emporiusm <at19eden@gmail.com>
2021-05-18 18:59:52 +0800

    --- Auto-Git Commit ---

--- get_airports.py
+++ get_airports.py
@@ -1,35 +1,18 @@
 # %%
 doc = 'Assets/airports.csv'
 import pandas as pd
-
 df = pd.read_csv(doc)
-df
-# %%
 df = df[df['type'] != 'closed']
-df
-# %%
 iata = df[['iata_code', 'name', 'iso_country']].dropna()
-# %%
 iata.columns = ['IATA', 'Name', 'Country']
 iata['ID'] = range(0,iata.shape[0])
 iata.set_index('ID', drop=False, inplace=True)
-# %%
-# %%
 iata['Name'] = iata['Country'] + ' - ' + iata['IATA'] + ' - ' + iata['Name']
-
-# %%
 lst = []
 for name, id in zip(iata['Name'], iata['ID']):
-    lst.append(dict({"ID":id,  "Name":name}))
-# %%
-iata
-
-# %%
+    lst.append(dict({"ID":f"i{str(id).zfill(6)}",  "Name":name}))
 import json
-
 with open('iata.json', 'w') as js:
     content = json.dumps(lst)
     js.write(content)
-# %%
-
 # %%
--- get_countries.py
+++ get_countries.py
@@ -7,9 +7,11 @@ filtered = df[['code', 'name', 'id']].copy()
 filtered['Countries'] = filtered['code'] + ' - ' + filtered['name']
 lst = []
 for id, name in zip(range(0,filtered.shape[0]), filtered['Countries']):
-    lst.append({"ID": id, "Countries": name})
+    lst.append({"ID": f"C{str(id).zfill(6)}", "Countries": name})
+
 import json
 content = json.dumps(lst, ensure_ascii=False)
 with open('iso3166.json', 'w', encoding='utf-8') as js:
     js.write(content)
+
 # %%
