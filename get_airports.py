f83f99ce551e34f7a9beae61c1bd845b83d9ed8b
Emporiusm <at19eden@gmail.com>
2021-05-22 09:34:14 +0800

    commit 2

--- get_airports.py
+++ get_airports.py
@@ -2,12 +2,16 @@
 doc = 'Assets/airports.csv'
 import pandas as pd
 df = pd.read_csv(doc)
+print(df.head())
+
 df = df[df['type'] != 'closed']
 iata = df[['iata_code', 'name', 'iso_country']].dropna()
 iata.columns = ['IATA', 'Name', 'Country']
+
+
 iata['ID'] = range(0,iata.shape[0])
 iata.set_index('ID', drop=False, inplace=True)
-iata['Name'] = iata['Country'] + ' - ' + iata['IATA'] + ' - ' + iata['Name']
+iata['Name'] = iata['IATA'] + ' - ' + iata['Name']  + ', ' + iata['Country'].str.upper()
 lst = []
 for name, id in zip(iata['Name'], iata['ID']):
     lst.append(dict({"ID":f"i{str(id).zfill(6)}",  "Name":name}))
@@ -15,10 +19,8 @@ import json
 with open('iata.json', 'w') as js:
     content = json.dumps(lst)
     js.write(content)
-# %%
 
-iata
-# %%
+
 fil = iata[(iata['Country'].isin(['CN', 'LK', 'FR', 'IN', 'BD'])) & (iata['Name'].str.contains('International'))]
 fil.to_excel('fil.xlsx')
 # %%
