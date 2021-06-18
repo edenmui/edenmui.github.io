363eba4fc57aa60b41d3dd1389a7b1ad5d117054
Emporiusm <at19eden@gmail.com>
2021-06-18 18:36:49 +0800

    commit

--- get_swift.py
+++ get_swift.py
@@ -13,8 +13,44 @@ df['Type'] = df['Branch'].apply(lambda x: 'H' if x == '-' else 'B')
 
 df.set_index('SWIFT', inplace=True, drop=False)
 
-df['SWIFT + NAME'] = df['SWIFT'] + ' - ' + df['Bank Name']
+target_countries = ['CN',
+ 'HK',
+ 'TR',
+ 'IN',
+ 'RS',
+ 'VG',
+ 'LU',
+ 'FR',
+ 'GB',
+ 'MA',
+ 'BD',
+ 'TW',
+ 'PK',
+ 'ID',
+ 'ES',
+ 'CA',
+ 'CZ',
+ 'SG',
+ 'GR',
+ 'VN',
+ 'EG',
+ 'KH',
+ 'US',
+ 'KR',
+ 'NL',
+ 'LK',
+ 'AE',
+ 'IL',
+ 'TH',
+ 'TN',
+ 'PL',
+ 'DK',
+ 'IT']
 
+df = df[df['ISO Country Code'].isin(target_countries)]
+
+df['SWIFT + NAME'] = df['SWIFT'] + ' - ' + df['Bank Name']
+# %%
 df = df.apply(lambda x: x.apply(lambda y: str(y).title()))
 import json
 banned = '''AD AI AG AW BS BH BZ BM BW VG KY CK CY DM ET GH GI GD GG IR IM JE LR LI MT MH MC MS NR BQ NU PA WS SM RS SC LC KN VC'''
@@ -23,7 +59,7 @@ df = df.query("`ISO Country Code` not in @banned")
 
 lst = []
 for id, swift_name in zip(df['ID'], df['SWIFT + NAME']):
-    lst.append({"ID":f"{str(id).zfill(6)}", "BANK": swift_name})
+    lst.append({"ID":f"{str(id).zfill(6)}", "BANK": swift_name.upper()})
 
 content = json.dumps(lst, ensure_ascii=False)
 with open('swift.json', 'w', encoding='utf-8') as js:
